d94706042d7e8d4d0e64bf7a1dff6ec14667aa52
ivaylo5ev <ivaylo5ev@gmail.com>
2019-09-30 13:00:06 +0300

    * updated paket command not to fire when the target project is not  a paket project

--- src/paket.fsx
+++ src/paket.fsx
@@ -9,27 +9,33 @@ open Fake.IO
 open Fake.Net
 
 module Paket =
+    let isPaketProject projectDir =
+        let paketReferencesFile = Path.combine projectDir "paket.references"
+        return Shell.testFile paketReferencesFile
     let paket retryCount paketVersion commands =
-        let rc = if (retryCount < 0) then 0 else retryCount
-        let paketEndpoint = sprintf "https://github.com/fsprojects/Paket/releases/download/%s/paket.exe" paketVersion
         let currentDir = Shell.pwd()
-        let mutable paketDir = Path.combine currentDir ".paket"
-        try
-            while (paketDir |> (Shell.testDir >> not)) do
-                Shell.cd "..\\";
-                paketDir <- Path.combine (Shell.pwd()) ".paket" 
-        finally
-            Shell.cd currentDir
-              
-        let paketExe = Path.combine paketDir "paket.exe"
-        if not (File.exists paketExe) then
-            Trace.trace "downloading Paket"
-            Http.downloadFile paketExe paketEndpoint
+        if isPaketProject currentDir then
+            let rc = if (retryCount < 0) then 0 else retryCount
+            let paketEndpoint = sprintf "https://github.com/fsprojects/Paket/releases/download/%s/paket.exe" paketVersion
+            let mutable paketDir = Path.combine currentDir ".paket"
+            try
+                while (paketDir |> (Shell.testDir >> not)) do
+                    Shell.cd "..\\";
+                    paketDir <- Path.combine (Shell.pwd()) ".paket" 
+            finally
+                Shell.cd currentDir
+                
+            let paketExe = Path.combine paketDir "paket.exe"
+            if not (File.exists paketExe) then
+                Trace.trace "downloading Paket"
+                Http.downloadFile paketExe paketEndpoint
+                |> ignore
+            else
+                Trace.trace "Paket already exists"
+            Trace.trace "Installing dependencies"
+            // run "paket.exe"
+            Command.RawCommand(paketExe, Arguments.OfArgs commands)
+            |> runRetry rc
             |> ignore
-        else
-            Trace.trace "Paket already exists"
-        Trace.trace "Installing dependencies"
-        // run "paket.exe"
-        Command.RawCommand(paketExe, Arguments.OfArgs commands)
-        |> runRetry rc
-        |> ignore
+        else 
+            ()
